6af9a2e1cae7b71e06650f5813ce7baa8107e45f
Sarah Tirey <sarahtirey@gmail.com>
2021-03-14 17:08:33 -0400

    working on syncing database. added db.js

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 let express = require('express');
 // create an instance of express to fire off a top-level express() function. This allow us to create an Express app.
 let app = express();
+// importing the route object that I created and store in a variable called mytime
+let mytime = require('./controllers/timecontroller')
 
 
-app.use('/test', function(req, res){
-    res.send('This is a message from the test endpoint on the server!')
-})
 
-
+// calling app.use in the first parameter to create a base URL called /mytime.
+// the 2nd paramter for the use function, I pass in mytime. All routes created in timecontroller.js file will be sub-routes.
+app.use('/mytime', mytime)
 
 
 
