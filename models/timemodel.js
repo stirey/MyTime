f6487d2b6c1345048e34c63f1fab90f2bc840747
Sarah Tirey <sarahtirey@gmail.com>
2021-03-17 19:37:13 -0400

    added the timemodel

---
+++ models/timemodel.js
@@ -0,0 +1,36 @@
+module.exports = function (sequelize, DataTypes) {
+
+    const WorkEntry = sequelize.define('workentry', {
+        
+        date: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {notEmpty: true}
+        },
+        clockin: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {notEmpty: true}
+        },
+        clockout: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {notEmpty: true}
+        },
+        task: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {notEmpty: true}
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        rateofpay: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {notEmpty: true}
+        }
+    })
+    return WorkEntry;
+}
