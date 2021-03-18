8880974ab12c5a8aa52bc69565b0c0ea300aaafd
Sarah Tirey <sarahtirey@gmail.com>
2021-03-17 20:01:06 -0400

    create workentry functions-successful work post

--- controllers/timecontroller.js
+++ controllers/timecontroller.js
@@ -4,6 +4,7 @@ let express = require('express');
 // we are using the express variable from above to access the Router() method. This will return a router object. 
 //https://expressjs.com/en/4x/api.html#router
 let router = express.Router();
+const WorkEntry = require('../db').import('../models/timemodel');
 
 // using the router object by using the router variable to get access into the Router() object methods.
 // .get is one of the methods in the object and I call it here. This method allows me to complete an HTTP GET request.
@@ -13,5 +14,24 @@ router.get('/practice', function(req, res) {
     // .send is an express method that can be called on the res object. The response parameter is a simple string.
     res.send('This is a practice route')
 })
+
+/*******************
+ * WORKENTRY CREATE*
+ *******************/
+
+router.post('/create', (req, res) => {
+    const work = {
+        date: req.body.workentry.date,
+        clockin: req.body.workentry.clockin,
+        clockout: req.body.workentry.clockout,
+        task: req.body.workentry.task,
+        description: req.body.workentry.description,
+        rateofpay: req.body.workentry.rateofpay,
+    }
+    WorkEntry.create(work)
+        .then(workentry => res.status(200).json(workentry))
+        .catch(err => res.status(500).json({ error: err}))
+})
+
 // exporting the module for usage outside the file. 
 module.exports = router
